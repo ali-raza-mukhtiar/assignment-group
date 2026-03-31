240a03ee0b74ae54d9a1c771871634c3744617ff
aliraza <muhammadalirazamukhtiar@gmail.com>
2026-04-01 00:02:49 +0500

    first commit

---
+++ file.py
@@ -0,0 +1,41 @@
+# monthly_salary = 50000
+# bonus = 10000
+# deduction = 5000
+
+# final_salary = monthly_salary + bonus - deduction
+
+# print("Final Salary:", final_salary)
+
+# math = 85
+# physics = 78
+# computer = 92
+
+# total_marks = math + physics + computer
+# average_marks = total_marks / 3
+
+# print("Total Marks:", total_marks)
+# print("Average Marks:", average_marks)
+
+
+# speed = 60
+# time = 2.5
+
+# distance = speed * time
+
+# print("Distance Traveled:", distance, "km")
+
+# pkr = 23000
+# rate = 280
+
+# usd = pkr // rate
+
+# print("Amount in USD:", usd)
+
+# items = 125
+# box_capacity = 12
+
+# full_boxes = items // box_capacity
+# remaining_items = items % box_capacity
+
+# print("Full Boxes:", full_boxes)
+# print("Remaining Items:", remaining_items)
