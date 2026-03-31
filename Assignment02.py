3d2530aa56a22c0855e357a0f37fd03e52f36866
aliraza <muhammadalirazamukhtiar@gmail.com>
2026-04-01 00:43:10 +0500

    Added new assignment

---
+++ Assignment02.py
@@ -0,0 +1,161 @@
+# Student marks
+# sub1 = 85
+# sub2 = 78
+# sub3 = 92
+
+# total = sub1 + sub2 + sub3
+# average = total / 3
+
+# print("Total Marks:", total)
+# print("Average Marks:", average)
+
+# Q2: Online Shopping Cart
+# shirt = 1200
+# shoes = 3500
+# discount = 500
+
+# total = shirt + shoes
+# final_bill = total - discount
+
+# print("Total Bill:", total)
+# print("Discount:", discount)
+# print("Final Bill:", final_bill)
+
+# Q3: Salary Calculation
+
+# basic_salary = 50000
+# bonus = 0.20 * basic_salary
+# tax = 0.10 * basic_salary
+
+# final_salary = basic_salary + bonus - tax
+
+# print("Basic Salary:", basic_salary)
+# print("Bonus:", bonus)
+# print("Tax:", tax)
+# print("Final Salary:", final_salary)
+
+# Q4: Temperature Conversion
+
+# celsius = 35
+# fahrenheit = (celsius * 9/5) + 32
+
+# print("Celsius:", celsius)
+# print("Fahrenheit:", fahrenheit)
+
+# Q5: Cricket Score Analysis
+
+# m1 = 45
+# m2 = 78
+# m3 = 66
+
+# total = m1 + m2 + m3
+# average = total / 3
+
+# print("Total Runs:", total)
+# print("Average Runs:", average)
+
+# Q6: Age Calculator
+
+# current_year = 2026
+# birth_year = 2007
+
+# age = current_year - birth_year
+
+# print("Age is:", age)
+
+# Q7: Rectangle Area & Perimeter
+
+# length = 12
+# width = 8
+
+# area = length * width
+# perimeter = 2 * (length + width)
+
+# print("Area:", area)
+# print("Perimeter:", perimeter)
+
+
+# Q8: Mobile Balance System
+
+# balance = 1000
+# call_charges = 150
+# internet_charges = 300
+
+# remaining = balance - call_charges - internet_charges
+
+# print("Remaining Balance:", remaining)
+
+# Q9: Fuel Consumption
+
+# total_fuel = 50
+# used_fuel = 18
+
+# remaining = total_fuel - used_fuel
+
+# print("Remaining Fuel:", remaining, "liters")
+
+
+# Q10: Exam Percentage
+
+# math = 88
+# english = 76
+# science = 91
+
+# total = math + english + science
+# percentage = (total / 300) * 100
+
+# print("Total Marks:", total)
+# print("Percentage:", percentage)
+
+# Q11: Restaurant Bill Split
+
+# total_bill = 4800
+# people = 6
+
+# per_person = total_bill / people
+
+# print("Each person pays:", per_person)
+
+
+# Q12: Electricity Bill
+
+# units = 250
+# cost_per_unit = 18
+# fixed_charges = 500
+
+# total_bill = (units * cost_per_unit) + fixed_charges
+
+# print("Total Electricity Bill:", total_bill)
+
+# Q13: Bank Transaction
+
+# balance = 10000
+# deposit = 5000
+# withdrawal = 3000
+
+# final_balance = balance + deposit - withdrawal
+
+# print("Final Balance:", final_balance)
+
+
+# Q14: Speed Calculation
+
+# distance = 150
+# time = 3
+
+# speed = distance / time
+
+# print("Speed:", speed, "km/h")
+
+# Q15: Simple Interest
+
+
+# principal = 10000
+# rate = 5
+# time = 2
+
+# simple_interest = (principal * rate * time) / 100
+# total_amount = principal + simple_interest
+
+# print("Simple Interest:", simple_interest)
+# print("Total Amount:", total_amount)
