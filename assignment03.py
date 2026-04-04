d3b494cd6dffa8024c42d6dbdae0e2f5e3e3036a
aliraza <muhammadalirazamukhtiar@gmail.com>
2026-04-04 22:31:34 +0500

    assignment 03

---
+++ assignment03.py
@@ -0,0 +1,42 @@
+# ✅ 1. Create a string with your full name and print first and last character
+
+# name = "Ali Khan"
+
+# print("First character:", name[0])
+# print("Last character:", name[-1])
+
+
+# ✅ 2. Apply .strip(), .lower(), .replace() on " Python is AWESOME "
+
+# text = " Python is AWESOME "
+
+# # strip()
+# text1 = text.strip()
+
+# # lower()
+# text2 = text1.lower()
+
+# # replace()
+# text3 = text2.replace("awesome", "great")
+
+# print("Original:", text)
+# print("After strip:", text1)
+# print("After lower:", text2)
+# print("After replace:", text3)
+
+# ✅ 3. Take user input of favorite language and display message
+
+
+# fav_lang = input("Enter your favorite programming language: ")
+
+# print("Your favorite language is", fav_lang)
+
+
+# ✅ 4. Count how many times "Python" appears
+
+
+# sentence = "I love Python because Python is powerful"
+
+# count = sentence.count("Python")
+
+# print("Python appears", count, "times")
